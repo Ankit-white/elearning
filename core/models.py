9dfd57e58d94f13d0a1ddaa4d609fa9b34b2c1f7
Ankit-white <ankitsaraf2134@gmail.com>
2026-04-27 17:50:37 +0530

    feat:updated

--- core/models.py
+++ core/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 class File(models.Model):
     title = models.CharField(max_length=100)
-    file = models.FileField(upload_to='uploads/')
+    file = CloudinaryField('file', resource_type='auto')
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
